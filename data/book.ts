1b304b9c47c35bdee330ca26f693c289c399f3b4
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-05-24 11:10:17 +0530

    easignore

--- data/book.ts
+++ data/book.ts
@@ -1,7 +1,6 @@
 import { DEFAULT_VOLUME_ID, getVolumeById } from "./volumes";
 
 export const BOOK_TITLE = "Shifa Shareef";
-export const SHIFA_PDF_ASSET = require("../assets/pdf/shifa-shareef-v1.pdf");
 
 // Compatibility exports for the current single-volume screens.
 export const TOTAL_PAGES = getVolumeById(DEFAULT_VOLUME_ID).totalPages;
