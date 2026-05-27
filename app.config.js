8eb11e5ef09491b9f6d7f2d40a6939c0750949eb
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-05-27 22:54:52 +0530

    android ci workflow

--- app.config.js
+++ app.config.js
@@ -36,7 +36,7 @@ export default {
       bundleIdentifier: getUniqueIdentifier(),
     },
     android: {
-      versionCode: 1,
+      versionCode: 2,
       adaptiveIcon: {
         foregroundImage: "./assets/images/icon.png",
       },
